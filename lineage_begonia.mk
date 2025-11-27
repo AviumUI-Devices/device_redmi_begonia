d9e364896daf309ab3e0b7042735ecac4c3f72eb
WuXing90 <2096477910@qq.com>
2025-11-28 04:57:32 +0800

    begonia: Bringup for AviumUI
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -15,24 +15,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-# Inherit some common Infinity stuff
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_DOESNT_LIKE_FLIPENDO := true
-TARGET_SUPPORTS_BLUR := false
-TARGET_HAS_UDFPS := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
 
-# Infinity-X Specific Flags
-INFINITY_BUILD_TYPE := OFFICIAL
-INFINITY_MAINTAINER := ShantanuPC
-WITH_GAPPS := true
-TARGET_BUILD_GOOGLE_TELEPHONY := false
-USE_MOTO_CALCULATOR := true
+# AviumUI Specific Flags
+AVIUM_VERSION_APPEND_TIME_OF_DAY := false
+WITH_GMS := false
+# TARGET_GMS_TYPE := MINI
+# TARGET_INCLUDE_GOOGLEIME := true
+# TARGET_GOOGLEIME_OVERRIDE_IME := true
+AVIUM_MAINTAINER := wuxinG90
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
-$(call inherit-product-if-exists, vendor/MiuiCameraLeica/config.mk)
 
 # Fix uses broken libraries
 RELAX_USES_LIBRARY_CHECK := true
@@ -49,7 +46,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := infinity_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
