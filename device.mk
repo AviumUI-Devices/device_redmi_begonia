d1c3e6f6e33f7beb851db04fce18f84b8aeac698
snnbyyds <snnbyyds@gmail.com>
2025-11-28 18:34:47 +0800

    begonia: Convert rootdir to init
    Use Android.bp as well.
    
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- device.mk
+++ device.mk
@@ -328,8 +328,10 @@ PRODUCT_PACKAGES += \
     init.project.rc \
     init.sensor_1_0.rc \
     fstab.mt6785 \
-    fstab.mt6785_ramdisk \
     ueventd.mt6785.rc
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/etc/fstab.mt6785:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6785
 
 # Radio
 PRODUCT_PACKAGES += \
