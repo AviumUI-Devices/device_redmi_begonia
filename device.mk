15acb33bd26b0f1fbe1fb7c93a2a0e8606a71911
Ghosuto <clash.raja10@gmail.com>
2026-01-17 20:06:52 +0530

    begonia: Import LunarisDolby * Imported from https://github.com/Pong-Development/hardware_dolby
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: kenway214 <kenway214@outlook.com>
    Change-Id: I9b31e444d2895a8c889030f738e9041e79fae3f6
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- device.mk
+++ device.mk
@@ -117,7 +117,7 @@ PRODUCT_PACKAGES += \
 
 # Dolby
 PRODUCT_PACKAGES += \
-    XiaomiDolby
+    LunarisDolby
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
