c8c3d1a438d7267f84daa6a8c288384c338c26a2
WuXing90 <2096477910@qq.com>
2026-05-21 19:07:50 +0800

    begonia: Drop libshims vtservice
    We don't need it
    
    Change-Id: Ifc5029d5bba65929132c8292644b52e21379b4a2
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>

--- device.mk
+++ device.mk
@@ -478,10 +478,6 @@ PRODUCT_PACKAGES += \
     libhidlbase \
     libunwindstack.vendor
 
-# MTK IMS
-PRODUCT_PACKAGES += \
-    libshim_vtservice
-
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
